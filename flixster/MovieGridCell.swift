0f37fa3dc2c0249d714cc840fb9dc9d326e460b9
Leroy Orr <leroyorr@Leroys-MacBook-Pro-4.local>
2020-02-02 22:41:57 -0500

    superhero screen

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Leroy Orr on 2/2/20.
+//  Copyright © 2020 Leroy Orr. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
